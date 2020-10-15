9f0000fa11871479ccc97cb97080a0a0eb3bd888
Andres Felipe Duarte Ochoa <aduarte@bancodebogota.com.co>
2020-10-15 09:49:31 -0500

    Inicializacion de Proyecto

---
+++ src/agario/Agario.java
@@ -0,0 +1,33 @@
+package agario;
+
+import model.DisplayGame;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JViewport;
+import javax.swing.ScrollPaneConstants;
+
+
+public class Agario {
+
+	public static boolean gamemode;
+	public static void main(String[] args) {
+		
+		JFrame frame= new JFrame("AGARIO INFORMATICA 1");
+		JScrollPane pane= new JScrollPane();
+		JViewport vport= new JViewport();
+		DisplayGame panel= new DisplayGame();
+		
+		panel.menu.setArgs(args);
+		vport.add(panel);
+		frame.setVisible(true);
+		pane.setViewport(vport);
+		vport.add(panel);
+		frame.add(pane);
+		frame.setSize(DisplayGame.WIDTH, DisplayGame.HEIGHT);
+		panel.setvPort(vport);
+		pane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+		pane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
